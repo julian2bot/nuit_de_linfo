f49ac651ae12ef1b62c65dd98f69fbc90826d783
julian2bot <marquesjulian26@gmail.com>
2024-03-28 19:33:58 +0100

    fichier de la nuit de l'info

---
+++ js.js
@@ -0,0 +1,10 @@
+
+function myFunction() {
+    var element = document.getElementById("image");
+    element.classList.toggle("style_img");
+  }
+
+setTimeout(() => {
+    myFunction();
+  }, "300000");
+  
